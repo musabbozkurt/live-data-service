7cd3d55f1b3b1e960698e2a0271451301fc64a5a
musab.bozkurt <musab.bozkurt@ozan.com>
2022-11-13 07:11:44 +0300

    add swagger annotations to ApiScoreBoardResponse to make doc understandable

--- src/main/java/com/sportradar/livedataservice/api/response/ApiScoreBoardResponse.java
+++ src/main/java/com/sportradar/livedataservice/api/response/ApiScoreBoardResponse.java
@@ -1,5 +1,6 @@
 package com.sportradar.livedataservice.api.response;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,17 +18,24 @@ import java.time.OffsetDateTime;
 @RequiredArgsConstructor
 public class ApiScoreBoardResponse {
 
+    @ApiModelProperty(value = "Id of score board")
     private Long id;
 
+    @ApiModelProperty(value = "Created date of score board")
     private OffsetDateTime createdDateTime;
 
+    @ApiModelProperty(value = "Last modified date of score board")
     private OffsetDateTime modifiedDateTime;
 
+    @ApiModelProperty(value = "Home Team Name", example = "TURKEY")
     private String homeTeamName;
 
+    @ApiModelProperty(value = "Away Team Name", example = "IRELAND")
     private String awayTeamName;
 
+    @ApiModelProperty(value = "Home Team Score", example = "0")
     private int homeTeamScore;
 
+    @ApiModelProperty(value = "Away Team Score", example = "0")
     private int awayTeamScore;
 }
