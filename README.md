b418b39d828efc64dc8469aa4fc1b1ac800fb30c
musab.bozkurt <musab.bozkurt@ozan.com>
2022-12-05 00:36:42 +0300

    Swagger is upgraded, docker-compose.yml, Grafana, and Prometheus are added.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
     <li><a href="#Tech_Stack">Tech Stack</a></li>
     <li><a href="#How_To_Run_And_Test_Application">How To Run And Test Application</a></li>
     <li><a href="#How_To_Run_And_Test_Dockerfile">How To Run And Test Application with Dockerfile (OPTIONAL)</a></li>
+    <li><a href="#How_To_Run_And_Test_Docker_Compose">How To Run And Test Application with docker-compose.yml (OPTIONAL)</a></li>
   </ol>
 </details>
 
@@ -31,7 +32,7 @@
     - url: jdbc:h2:mem:SPORT_RADAR;DB_CLOSE_DELAY=-1
     - Default H2 Database url: http://localhost:8080/h2-console
 - Swagger2 - For API Local Testing
-    - Default Swagger url: http://localhost:8080/swagger-ui.html
+    - Default Swagger url: http://localhost:8080/swagger-ui/
 - Flyway for database migration
 - Centralize exception handling by ControllerAdvice
 - Mapstruct to map different type of objects to each other
@@ -50,7 +51,7 @@
 
 *** Test the application by following these steps.
 
-$ Default Swagger url: http://localhost:8080/swagger-ui.html
+$ Default Swagger url: http://localhost:8080/swagger-ui/
 $ Default H2 Database url: http://localhost:8080/h2-console
 
 $ Use Swagger and click on Try it out button and fill the input payload according to the contract.
@@ -64,17 +65,42 @@ $ Use Swagger and click on Try it out button and fill the input payload accordin
 
 ```
 1 - cd live-data-service
-2 - mvn clean install --------THIS IS MUST---------
-3 - docker build -t springio/live-data-service .
-4 - docker run -p 8080:8080 springio/live-data-service
+2 - mvn clean install or mvn clean package --------THIS IS MUST---------
+3 - docker build -t mb/live-data-service .
+4 - docker run -p 8080:8080 mb/live-data-service
 
 *** Test the application by following these steps.
 
-$ Default Swagger url: http://localhost:8080/swagger-ui.html
+$ Default Swagger url: http://localhost:8080/swagger-ui/
 $ Default H2 Database url: http://localhost:8080/h2-console
 
 $ Use Swagger and click on Try it out button and fill the input payload according to the contract.
 ```
+
+### How_To_Run_And_Test_Docker_Compose
+
+- Please follow the following steps, if you want to build and run docker-compose.yml
+
+```
+1 - cd live-data-service
+2 - mvn clean install or mvn clean package --------THIS IS MUST---------
+3 - docker build -t mb/live-data-service .
+4 - docker-compose up -d
+
+*** Test the application by following these steps.
+
+$ Default Swagger url: http://localhost:8080/swagger-ui/
+$ Default H2 Database url: http://localhost:8080/h2-console
+
+$ Use Swagger and click on Try it out button and fill the input payload according to the contract.
+```
+
+- docker-compose.yml contains Grafana and Prometheus to track metrics 
+  - Actuator url: http://localhost:8080/actuator/prometheus
+  - Prometheus url: http://localhost:9090/graph
+  - Grafana url: http://localhost:3000/login
+      - username: admin
+      - password: admin
 
 ### Reference Documentation
 
@@ -92,4 +118,3 @@ The following guides illustrate how to use some features concretely:
 * [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
 * [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
 * [Building REST services with Spring](https://spring.io/guides/tutorials/rest/)
-
--- src/main/java/com/sportradar/livedataservice/config/SwaggerConfig.java
+++ src/main/java/com/sportradar/livedataservice/config/SwaggerConfig.java
@@ -1,24 +1,88 @@
 package com.sportradar.livedataservice.config;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.*;
+import java.util.function.Predicate;
+
 @Configuration
 @EnableSwagger2
+@ConfigurationProperties(prefix = "swagger.documentation")
+@EnableConfigurationProperties({SwaggerConfig.class, SwaggerConfig.SwaggerServices.class})
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.sportradar"))
-                .paths(PathSelectors.any())
-                .build();
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicate.not(PathSelectors.regex("/error")))
+                .paths(Predicate.not(PathSelectors.regex("/actuator.*")))
+                .build()
+                .produces(new HashSet<>(Collections.singletonList(MediaType.APPLICATION_JSON_VALUE)));
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(WebMvcEndpointHandlerMapping.class)
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
+                                                                         ServletEndpointsSupplier servletEndpointsSupplier,
+                                                                         ControllerEndpointsSupplier controllerEndpointsSupplier,
+                                                                         EndpointMediaTypes endpointMediaTypes,
+                                                                         CorsEndpointProperties corsProperties,
+                                                                         WebEndpointProperties webEndpointProperties,
+                                                                         Environment environment) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping, null);
+    }
+
+
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+    }
+
+    @Getter
+    @Setter
+    public List<SwaggerServices> services;
+
+    @Data
+    @ToString
+    @EnableConfigurationProperties
+    @ConfigurationProperties(prefix = "swagger.documentation.services")
+    public static class SwaggerServices {
+        private String name;
+        private String url;
+        private String version;
     }
 
 }
