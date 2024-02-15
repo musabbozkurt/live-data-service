45f146addc800c8c645b1483db482dcb42e8b6d3
musab.bozkurt <musab.bozkurt@ozan.com>
2024-02-16 02:36:48 +0300

    rearrange code

--- src/main/java/com/mb/livedataservice/util/LiveDataConstants.java
+++ src/main/java/com/mb/livedataservice/util/LiveDataConstants.java
@@ -1,10 +1,10 @@
 package com.mb.livedataservice.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class LiveDataConstants {
 
     public static final String DEFAULT_ID_GENERATOR_NAME = "default_sequence_generator";
-
-    private LiveDataConstants() {
-
-    }
 }
